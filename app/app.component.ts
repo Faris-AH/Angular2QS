ab418cbb8c0689d100cd60f34ff74a467ce4082d
faris <faris.hammed@gmail.com>
2016-10-17 16:26:35 +0500

    * Angular 2 Quick Start

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by faris on 10/17/2016.
+ */
+import { Component } from '@angular/core';
+@Component({
+    selector: 'my-app',
+    template: '<h1>My First Angular App</h1>'
+})
+export class AppComponent { }
